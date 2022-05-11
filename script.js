2807b6b82f5ff3c7ffb024d714efa8ce6093fcff
TerraPlayer1 <terragamer70@gmail.com>
2022-05-11 08:14:36 +0300

    Change name of number variables

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const display = document.querySelector(".screen")
 const operator = document.querySelectorAll(".opr")
 const calc = document.querySelector(".eval")
 
-let preOprNum = "";
-let postOprNum = "";
+let firstNum = "";
+let secNum = "";
 
 num.forEach(n => {
   n.addEventListener("click", function click(e){
     if(oprPresent) {
-      postOprNum += n.textContent;
-      display.textContent = postOprNum;
-      console.log(preOprNum,postOprNum)
+      secNum += n.textContent;
+      display.textContent = secNum;
+      console.log(firstNum,secNum)
     } else {
-    preOprNum += n.textContent;
-    display.textContent = preOprNum;
-    console.log(preOprNum,postOprNum)
+    firstNum += n.textContent;
+    display.textContent = firstNum;
+    console.log(firstNum,secNum)
     };
   });
 });
@@ -38,10 +38,10 @@ let total = 0
 calc.addEventListener("click", equal);
 
 function equal(){
-  display.textContent = preOprNum = total = operate(oprType, parseFloat(preOprNum), parseFloat(postOprNum));
-  postOprNum = "";
+  display.textContent = firstNum = total = operate(oprType, parseFloat(firstNum), parseFloat(secNum));
+  secNum = "";
 }
-
+// TODO: Deal with nan when equal is called with no second number present
 
 
 function add(a, b){
